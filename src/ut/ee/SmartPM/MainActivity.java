b3611e1be3f7b85d623b2d9bace992742328fa42
Patris Halapuu <p2tris@gmail.com>
2014-10-23 16:14:00 +0200

    Adds options menu

--- src/ut/ee/SmartPM/MainActivity.java
+++ src/ut/ee/SmartPM/MainActivity.java
@@ -13,6 +13,8 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -196,5 +198,32 @@ public class MainActivity extends Activity {
 		
 		super.onDestroy();
 	}
+	
+	@Override  
+    public boolean onCreateOptionsMenu(Menu menu) {  
+        // Inflate the menu; this adds items to the action bar if it is present.  
+        getMenuInflater().inflate(R.menu.menu_main, menu);//Menu Resource, Menu  
+        return true;  
+    }  
+      
+    @Override  
+    public boolean onOptionsItemSelected(MenuItem item) {  
+        switch (item.getItemId()) {  
+            case R.id.item1:  
+              Toast.makeText(getApplicationContext(),"Item 1 Selected",Toast.LENGTH_LONG).show();  
+            return true;     
+  
+            case R.id.item2:  
+                Toast.makeText(getApplicationContext(),"Item 2 Selected",Toast.LENGTH_LONG).show();  
+              return true;     
+              
+            case R.id.item3:  
+                Toast.makeText(getApplicationContext(),"Item 3 Selected",Toast.LENGTH_LONG).show();  
+              return true;     
+  
+              default:  
+                return super.onOptionsItemSelected(item);  
+        }  
+    }  
 
 }
