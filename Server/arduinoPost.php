bc5d83503b3a0ea7b87ec2d6cb1cf0878cc747fe
Patris Halapuu <p2tris@gmail.com>
2015-01-11 12:59:46 +0100

    Fixes file naming

--- Server/arduinoPost.php
+++ Server/arduinoPost.php
@@ -1,13 +1,13 @@
 <?php
 
 $timestamp = time();
-$filename = 'arduinorules/humidrules'.$timestamp.'.xml';
+$filename = 'arduinorules/'.$_POST['sensor_type'].'rules'.$timestamp.'.xml';
 
 
 $f = fopen($filename,"a");
-fprintf($f, '<?xml version="1.0" encoding="UTF-8"?><humid_type><lib name="humid lib" keyword="'.$_POST['sensor_type'].'" url="http://smartpm.cloudapp.net/SmartPM_libHumid.apk" />');
+fprintf($f, '<?xml version="1.0" encoding="UTF-8"?><'.$_POST['sensor_type'].'_type><lib name="arduino lib" keyword="'.$_POST['sensor_type'].'" url="http://smartpm.cloudapp.net/SmartPM_libHumid.apk" />');
 fprintf($f, stripslashes($_POST['rules'] ));
-fprintf($f, '</humid_type>');
+fprintf($f, '</'.$_POST['sensor_type'].'_type>');
 fclose($f);
 
 echo 'http://smartpm.cloudapp.net/'.$filename;
